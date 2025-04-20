ca25c9c69f77cf6724c927b85306f913eda1f5e8
leeivana <ivana.wy.lee@gmail.com>
2025-04-20 17:48:48 +0900

    Updates loading animation color

--- src/components/LoadingAnimation.tsx
+++ src/components/LoadingAnimation.tsx
@@ -3,9 +3,9 @@ import React from "react";
 const LoadingAnimation = () => {
     return (
         <div className="flex justify-center" aria-label="読み込み中">
-            <div className="animate-ping h-2 w-2 bg-blue-600 rounded-full"></div>
-            <div className="animate-ping h-2 w-2 bg-blue-600 rounded-full mx-4"></div>
-            <div className="animate-ping h-2 w-2 bg-blue-600 rounded-full"></div>
+            <div className="animate-ping h-2 w-2 bg-zinc-300 rounded-full"></div>
+            <div className="animate-ping h-2 w-2 bg-zinc-300 rounded-full mx-4"></div>
+            <div className="animate-ping h-2 w-2 bg-zinc-300 rounded-full"></div>
         </div>
     );
 };
